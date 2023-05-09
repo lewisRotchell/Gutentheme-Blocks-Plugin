108cc9dc807eebb17f147ef3d4cb6e71a2e90a03
Lewis Rotchell <rotchell92@gmail.com>
2023-05-09 16:43:02 +0100

    Added gitkeep check

--- includes/register-blocks.php
+++ includes/register-blocks.php
@@ -1,12 +1,13 @@
 <?php
 
-function pb_register_blocks() {
-    foreach( scandir(PB_PLUGIN_PATH . 'build/blocks') as $file ) {
-        if($file === '.' || $file === '..') continue;
+function pb_register_blocks()
+{
+    foreach (scandir(PB_PLUGIN_PATH . 'build/blocks') as $file) {
+        if ($file === '.' || $file === '..' || $file === '.gitkeep') continue;
         $options = [];
-        if( file_exists(PB_PLUGIN_PATH. "src/blocks/$file/render.php") ) {
-            $options['render_callback'] = $file.'_render_cb';
+        if (file_exists(PB_PLUGIN_PATH . "src/blocks/$file/render.php")) {
+            $options['render_callback'] = $file . '_render_cb';
         }
-        register_block_type( PB_PLUGIN_PATH . "build/blocks/$file", $options );
+        register_block_type(PB_PLUGIN_PATH . "build/blocks/$file", $options);
     }
 }
