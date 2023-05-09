488b098367b8a96cbe727000a961aaaf312c7ee2
Lewis Rotchell <rotchell92@gmail.com>
2023-05-09 17:45:22 +0100

    added new create block script and also modified register blocks function

--- includes/register-blocks.php
+++ includes/register-blocks.php
@@ -2,12 +2,14 @@
 
 function pb_register_blocks()
 {
-    foreach (scandir(PB_PLUGIN_PATH . 'build/blocks') as $file) {
-        if ($file === '.' || $file === '..' || $file === '.gitkeep') continue;
-        $options = [];
-        if (file_exists(PB_PLUGIN_PATH . "src/blocks/$file/render.php")) {
-            $options['render_callback'] = $file . '_render_cb';
+    if (is_dir(PB_PLUGIN_PATH . 'build/blocks')) {
+        foreach (scandir(PB_PLUGIN_PATH . 'build/blocks') as $file) {
+            if ($file === '.' || $file === '..' || $file === '.gitkeep') continue;
+            $options = [];
+            if (file_exists(PB_PLUGIN_PATH . "src/blocks/$file/render.php")) {
+                $options['render_callback'] = $file . '_render_cb';
+            }
+            register_block_type(PB_PLUGIN_PATH . "build/blocks/$file", $options);
         }
-        register_block_type(PB_PLUGIN_PATH . "build/blocks/$file", $options);
     }
 }
